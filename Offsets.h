f932171d4bd1717624f96b20cb982fb9052bde07
Ali Bin Mohammad <43062310+BINM7MD@users.noreply.github.com>
2022-05-12 20:37:05 +0300

    PUBGM-2.0.0-Offsets
    PUBGM-2.0.0-Offsets

---
+++ Offsets.h
@@ -0,0 +1,39 @@
+Gname -> 0x7827A58
+GWorld -> 0x7B28F74
+View World -> 0x7B06640
+
+const off_est RootComponent = 0x16c;
+STExtraWeapon {
+const off_est CurrentReloadWeapon = 0x1e50;
+const off_est ShootWeaponEntity = 0xbb0;
+const off_est BulletFireSpeed = 0x404;
+const off_est ShootInterval = 0x428;
+const off_est AccessoriesVRecoilFactor = 0x858;
+const off_est AccessoriesHRecoilFactor = 0x85c;
+const off_est ShotGunVerticalSpread = 0x8ac;
+const off_est ShotGunHorizontalSpread = 0x8b0;
+const off_est GameDeviationFactor = 0x8b4;
+const off_est RecoilKickADS = 0x960;
+const off_est AnimationKick = 0x978;
+}
+STExtraCharacter
+{
+	const off_est TeamID = 0x69c;
+		const off_est IsAi = 0x714;
+		const off_est IsAlive = 0x998;
+		const off_est Health = 0x9a4;
+		const off_est HealthMax = 0x9a8;
+		const off_est NearDeathBreath = 0x1214;
+		const off_est PlayerName = 0x668;
+		const off_est Nation = 0x674;
+		const off_est CurBulletNumInClip = 0xaa8;
+		const off_est CurMaxBulletNumInOneClip = 0xabc;
+		const off_est CurrentStates = 0xb08;
+}
+ VehicleCommonComponent {
+    const off_est VehicleCommon = 0x6dc;
+    const off_est HPMax = 0x1a4;
+    const off_est HP = 0x1a8;
+    const off_est FuelMax = 0x1bc;
+    const off_est Fuel = 0x1c0;
+}
